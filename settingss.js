4368e39cf108d2027831a80b2079b2c3edd3265a
farooqbot <85386300+farooqbot@users.noreply.github.com>
2025-07-27 14:05:08 +0530

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with malvin~
-  OWNER_NUMBER: "263714757857", // PUT OWNER NUMBER HERE
+  SESSION_ID: "malvin~xiVnzaDZ#NuiWKbnVHx8prCkC9P_TicTeujaKm1X7HtMB7tiVw8A",  // add your Session Id make sure it starts with malvin~
+  OWNER_NUMBER: "917356296007", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
